5e07053f9302d35330d1fcda24db3106df953cc6
francescobarbera <francesco.barbera@mondora.com>
2016-05-09 14:31:53 +0200

    adds cmds to publish package

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export {default as FeatureCurrent} from './components/FeatureCurrent';
+export {default as FeatureNext} from './components/FeatureNext';
+export {default as FeatureSwitch} from './components/FeatureSwitch';
