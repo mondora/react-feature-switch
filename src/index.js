a1b1a9cd90cdc43cb99d65fb74fbe86abf0bcae9
francescobarbera <francesco.barbera@mondora.com>
2016-05-09 16:28:54 +0200

    exports FeatureSwitchRoot

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as FeatureCurrent} from './components/FeatureCurrent';
 export {default as FeatureNext} from './components/FeatureNext';
 export {default as FeatureSwitch} from './components/FeatureSwitch';
+export {default as FeatureSwitchRoot} from './components/FeatureSwitchRoot';
